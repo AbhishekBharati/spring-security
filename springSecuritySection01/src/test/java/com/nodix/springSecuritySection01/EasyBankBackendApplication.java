3c322d19bb3332534ef467b80a61c1860902846c
AbhishekBharati <bharatiabhishek30@gmail.com>
2025-09-22 14:36:11 +0530

    First Commit

---
+++ springSecuritySection01/src/test/java/com/nodix/springSecuritySection01/EasyBankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nodix.springSecuritySection01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBankBackendApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
