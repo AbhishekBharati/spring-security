97012045a66acbe23f242500045e85d60c9a9ecc
AbhishekBharati <bharatiabhishek30@gmail.com>
2025-09-23 12:43:00 +0530

    Wrote few controllers to see how spring security works

--- springSecuritySection01/src/main/java/com/nodix/springSecuritySection01/controller/NoticesController.java
+++ springSecuritySection01/src/main/java/com/nodix/springSecuritySection01/controller/NoticesController.java
@@ -4,11 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class WelcomeController {
+public class NoticesController {
 
-    @GetMapping("/welcome")
-    public String sayWelcome(){
-        return "Welcome to Spring Application without securityyy";
+    @GetMapping("/notices")
+    public String getNotices(){
+        return "Here are the Notices Details from the DB.";
     }
-
 }
