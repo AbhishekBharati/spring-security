b0bd6db9063bee1ca3da05d35f8e5094e35efa72
AbhishekBharati <bharatiabhishek30@gmail.com>
2025-09-23 16:57:32 +0530

    Understood few extra things in Configuration like InMemoryUserDetails and PasswordEncoder and HaveIBeenPawned

--- springSecuritySection01/src/main/java/com/nodix/springSecuritySection01/config/ProjectSecurityConfig.java
+++ springSecuritySection01/src/main/java/com/nodix/springSecuritySection01/config/ProjectSecurityConfig.java
@@ -3,10 +3,18 @@ package com.nodix.springSecuritySection01.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -27,5 +35,33 @@ public class ProjectSecurityConfig {
         http.formLogin(flc -> flc.disable());
         http.httpBasic(Customizer.withDefaults());
         return (SecurityFilterChain)http.build();
+    }
+
+//    Now suppose I want to add many users over here itself then we will be doing it this way.
+    @Bean
+    public UserDetailsService userDetailsService(){
+//        Here we're putting the bcrypt hashed value so that no one in github can determine what our password is :-
+        UserDetails user = User.withUsername("nodix")
+                .password("{bcrypt}$2a$12$KTFzTpiHvl/UoCaUqbF3QOETfXZQpe2iZfb4h6jrdXV5nzF0iyvX.")
+                .authorities("read")
+                .build();
+//        By noop We're specifying that we don't wanna encode our password and want to keep it as plain text.
+        UserDetails admin = User.withUsername("admin").password("{noop}1212").authorities("admin").build();
+//        This InMemoryUserDetailsManager is implemented by SpringTeam and it helps us to store the users in Memory
+//        Also This will store the users in a HashMap you can go inside and see it's implementation.
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+//    Because of this bean we don't need to write {noop} anymore before the password coz this time it'll use the encryption method that this bean returns.
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+//        This FactoryMethod will by default consider bcrypt for the Password Hashing purposes.
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+//    This bean Checks the password and let us know that If that password is ever been Leaked in a DataBreach or not.
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker(){
+        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 }
