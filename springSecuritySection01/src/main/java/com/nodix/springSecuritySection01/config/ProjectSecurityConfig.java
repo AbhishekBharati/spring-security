22d60c7508c5088c1d10f7406ac385e795defe76
AbhishekBharati <bharatiabhishek30@gmail.com>
2025-09-23 14:37:26 +0530

    Configured the pages which requires the security and the pages which doesn't requires the security

--- springSecuritySection01/src/main/java/com/nodix/springSecuritySection01/config/ProjectSecurityConfig.java
+++ springSecuritySection01/src/main/java/com/nodix/springSecuritySection01/config/ProjectSecurityConfig.java
@@ -13,8 +13,18 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated());
-        http.formLogin(Customizer.withDefaults());
+//        This will permit all the requests to the api without authentication
+//        http.authorizeHttpRequests((requests) -> ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).permitAll());
+//        This will deny all the requests to the API irrespective of authentication.
+//        http.authorizeHttpRequests((requests) -> ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).denyAll());
+        http.authorizeHttpRequests((requests) -> (requests.
+                requestMatchers("/myBalance", "/myAccount", "/myLoans", "myCards")).authenticated()
+                .requestMatchers("/notices", "/contact", "/error").permitAll()
+        );
+//        Form Login is responsible to give us that login form in beginning.
+//        http.formLogin(Customizer.withDefaults());
+//        This will disable the Form Login.
+        http.formLogin(flc -> flc.disable());
         http.httpBasic(Customizer.withDefaults());
         return (SecurityFilterChain)http.build();
     }
